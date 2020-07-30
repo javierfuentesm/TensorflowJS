aad32b0250056f43d0b627242c3aae831e698fe7
Javier <javier.fuentesm@hotmail.com>
2020-07-29 23:37:37 -0400

    Transferencia de conocimiento a un modelo de clasificación de imágenes

--- clasificador.js
+++ clasificador.js
@@ -1,17 +1,47 @@
-let net;
+let net, webcam;
 const imgEl = document.getElementById('img')
 const descripcion = document.getElementById('descripcion_imagen')
+const webcamElement = document.getElementById('webcam')
+const consola = document.getElementById('consola')
+const consola2 = document.getElementById('consola2')
+const classifier = knnClassifier.create()
 
 const app = async () => {
     net = await mobilenet.load()
     displayImagePrediction()
+    webcam = await tf.data.webcam(webcamElement);
+
+    while (true) {
+        const img = await webcam.capture()
+        const result = await net.classify(img)
+        consola.innerHTML = `prediction ${result[0].className}  probability: ${result[0].probability}`
+        const activation = net.infer(img,"conv_preds")
+        try {
+            const result2 = await classifier.predictClass(activation)
+            const classes = ["Untrained","Airpods","Javier","OK","Rock"]
+            consola2.innerHTML =`Prediccion personalizada ${classes[result2.label]} Probabilidad ${result2.confidences[result2.label]}`
+        }catch (e) {
+            consola2.innerText="Untrained"
+        }
+        img.dispose()
+        await tf.nextFrame()
+    }
+
 }
 
 imgEl.onload = async () => {
     displayImagePrediction()
 }
 
-const cambiarImagen = async ()=>{
+const addExample = async (classId) => {
+    const img = await webcam.capture()
+    const activation = net.infer(img, true)
+    classifier.addExample(activation, classId)
+    img.dispose()
+
+}
+
+const cambiarImagen = async () => {
     let count = 0;
     count++
     imgEl.src = 'https://picsum.photos/200/300?random=' + count
