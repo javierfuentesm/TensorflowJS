59cbbd71bbe8db24209efd42970d2add4a8b92f2
Javier <javier.fuentesm@hotmail.com>
2020-08-11 11:10:19 -0400

    Visualizando la información

--- linear.js
+++ linear.js
@@ -1,8 +1,23 @@
+const plot = async(pointsArray, featureName)=>{
+    tfvis.render.scatterplot(
+        { name: `${featureName} vs House Price`},
+        { values: [pointsArray], series:['original']},
+        {
+            xLabel: featureName,
+            yLabel:'Price'
+        }
+    )
+}
+
 const run = async () => {
     const houseSalesDataset = tf.data.csv("http://127.0.0.1:5500/kc_house_data.csv")
     const sampleDataset = houseSalesDataset.take(10)
     const dataArray = await sampleDataset.toArray()
-    console.log(dataArray)
+    const points = houseSalesDataset.map(record => ({
+        x: record.sqft_living,
+        y: record.price
+    }))
+    plot(await points.toArray(),'Square feet' )
 }
 
 run()
