0a2746882f9393b2a71bfff8c68d32147fecc9cc
Javier <javier.fuentesm@hotmail.com>
2020-08-17 12:42:32 -0400

    splitting into training and testing data

--- linear.js
+++ linear.js
@@ -20,31 +20,39 @@ const normalise = (tensor)=>{
 
 }
 const run = async () => {
+    //Import from csv
     const houseSalesDataset = tf.data.csv("http://127.0.0.1:5500/kc_house_data.csv")
-    const sampleDataset = houseSalesDataset.take(10)
-    const dataArray = await sampleDataset.toArray()
-    const points = houseSalesDataset.map(record => ({
+
+    //Extract x and y values
+    const pointsDataset = houseSalesDataset.map(record => ({
         x: record.sqft_living,
         y: record.price
     }))
-    plot(await points.toArray(),'Square feet' )
+    const points = await pointsDataset.toArray()
+    if(points.length %2 !== 0){
+        points.pop()
+    }
+    tf.util.shuffle(points)
 
-    //Features (inputs)
-    const featureValues = await points.map(p=>p.x).toArray()
+    plot(points,'Square feet' )
+
+    //Extract Features (inputs)
+    const featureValues = await points.map(p=>p.x)
     const featureTensor = tf.tensor2d(featureValues,[featureValues.length,1])
 
-    const labelValues = await points.map(p=>p.y).toArray()
+    //Extract Labels (outputs)
+
+    const labelValues = await points.map(p=>p.y)
     const labelTensor = tf.tensor2d(labelValues,[labelValues.length,1])
 
-    featureTensor.print()
-    labelTensor.print()
-
+//Normalise features and labels
     const normaliseFeatureTensor = normalise(featureTensor)
     const normaliseLabelTensor = normalise(labelTensor)
-    normaliseFeatureTensor.tensor.print()
-    normaliseLabelTensor.tensor.print()
 
-    denormalise(normaliseFeatureTensor.tensor,normaliseFeatureTensor.min,normaliseFeatureTensor.max).print()
+    const [trainingFeatureTensor,testingFeatureTensor]= tf.split(normaliseFeatureTensor.tensor,2)
+    const [trainingLabelTensor,testingLabelTensor]= tf.split(normaliseLabelTensor.tensor,2)
+    trainingFeatureTensor.print(true)
+    //denormalise(normaliseFeatureTensor.tensor,normaliseFeatureTensor.min,normaliseFeatureTensor.max).print()
 }
 
 run()
