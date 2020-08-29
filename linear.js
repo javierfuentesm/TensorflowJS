bde582411e720c31b1be06f8bd12061deb9ccb0f
Javier <javier.fuentesm@hotmail.com>
2020-08-29 19:02:15 -0400

    Creating layes in tensorflowjs

--- linear.js
+++ linear.js
@@ -1,23 +1,32 @@
-const plot = async(pointsArray, featureName)=>{
+const plot = async (pointsArray, featureName) => {
     tfvis.render.scatterplot(
-        { name: `${featureName} vs House Price`},
-        { values: [pointsArray], series:['original']},
+        {name: `${featureName} vs House Price`},
+        {values: [pointsArray], series: ['original']},
         {
             xLabel: featureName,
-            yLabel:'Price'
+            yLabel: 'Price'
         }
     )
 }
-const denormalise =(tensor,min,max)=>{
+const denormalise = (tensor, min, max) => {
     return tensor.mul(max.sub(min)).add(min)
 }
-const normalise = (tensor)=>{
+const normalise = (tensor) => {
     const min = tensor.min()
     const max = tensor.max()
 
-    return {tensor:tensor.sub(min).div(max.sub(min)),min,max}
+    return {tensor: tensor.sub(min).div(max.sub(min)), min, max}
+}
 
-
+const createModel = () => {
+    const model = tf.sequential()
+    model.add(tf.layers.dense({
+        units: 1,
+        useBias: true,
+        activation: 'linear',
+        inputDim: 1
+    }))
+    return model
 }
 const run = async () => {
     //Import from csv
@@ -29,30 +38,32 @@ const run = async () => {
         y: record.price
     }))
     const points = await pointsDataset.toArray()
-    if(points.length %2 !== 0){
+    if (points.length % 2 !== 0) {
         points.pop()
     }
     tf.util.shuffle(points)
 
-    plot(points,'Square feet' )
+    plot(points, 'Square feet')
 
     //Extract Features (inputs)
-    const featureValues = await points.map(p=>p.x)
-    const featureTensor = tf.tensor2d(featureValues,[featureValues.length,1])
+    const featureValues = await points.map(p => p.x)
+    const featureTensor = tf.tensor2d(featureValues, [featureValues.length, 1])
 
     //Extract Labels (outputs)
 
-    const labelValues = await points.map(p=>p.y)
-    const labelTensor = tf.tensor2d(labelValues,[labelValues.length,1])
+    const labelValues = await points.map(p => p.y)
+    const labelTensor = tf.tensor2d(labelValues, [labelValues.length, 1])
 
 //Normalise features and labels
     const normaliseFeatureTensor = normalise(featureTensor)
     const normaliseLabelTensor = normalise(labelTensor)
 
-    const [trainingFeatureTensor,testingFeatureTensor]= tf.split(normaliseFeatureTensor.tensor,2)
-    const [trainingLabelTensor,testingLabelTensor]= tf.split(normaliseLabelTensor.tensor,2)
+    const [trainingFeatureTensor, testingFeatureTensor] = tf.split(normaliseFeatureTensor.tensor, 2)
+    const [trainingLabelTensor, testingLabelTensor] = tf.split(normaliseLabelTensor.tensor, 2)
     trainingFeatureTensor.print(true)
     //denormalise(normaliseFeatureTensor.tensor,normaliseFeatureTensor.min,normaliseFeatureTensor.max).print()
+
+    const model = createModel()
 }
 
 run()
